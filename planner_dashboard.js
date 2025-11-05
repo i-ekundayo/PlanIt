f076e725a8da3d06de70d479cf29a3c1c9ea8a32
Israel <isekundayo700@gmail.com>
2025-11-05 07:14:54 +0100

    removed excess line

--- planner_dashboard.js
+++ planner_dashboard.js
@@ -2,7 +2,5 @@ const menu = document.querySelector(".header__search img");
 const container = document.querySelector(".container");
 
 menu.addEventListener("click", () => {
-  console.log('clicked');
-  
   container.classList.toggle("collapsed");
 });
