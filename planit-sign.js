8370cf008fca13cf3427313c48ea6a4ee14012b5
Israel <isekundayo700@gmail.com>
2025-11-07 14:53:44 +0100

    worked on the message collaboration screen

--- planit-sign.js
+++ planit-sign.js
@@ -19,3 +19,42 @@ function togglePassword(inputId) {
     icon.className = "ph ph-eye";
   }
 }
+
+
+const fullName = document.getElementById("fnm");
+const email = document.getElementById("em");
+const password = document.getElementById("ps");
+
+
+// Creating a POST request for Signup
+const signUp = async () => {
+  try {
+    const response = await fetch(
+      "https://planit-production-e550.up.railway.app/api/auth/signup",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email.value,
+          password: password.value,
+          fullName: fullName.value,
+          role: "",
+          phoneNumber: "",
+        }),
+      }
+    );
+
+    console.log('clicked');
+    
+    console.log(response);
+    const data = await response.json();
+    console.log("✅ Success:", data); 
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
+
+
+signUp();
