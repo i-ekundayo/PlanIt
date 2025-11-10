a4a339aec82d7372a1130b78d9e1bd5307ea1250
Israel <isekundayo700@gmail.com>
2025-11-10 21:18:25 +0100

    worked on the vendor profile

--- planner_dashboard.js
+++ planner_dashboard.js
@@ -1,6 +1,39 @@
 const menu = document.querySelector(".header__search img");
 const container = document.querySelector(".container");
+const token = localStorage.getItem("token");
+
+
+
 
 menu.addEventListener("click", () => {
   container.classList.toggle("collapsed");
 });
+
+const userProfile = async () => {
+  try {
+    const response = await fetch(
+      "https://planit-production-e550.up.railway.app/api/auth/me",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+      }
+    );
+    const data = await response.json();
+
+    console.log(data);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    document.querySelector(".planner__name").textContent = data.fullName;
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+userProfile();
